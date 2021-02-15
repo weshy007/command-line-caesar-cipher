7c5a0f5b2b6b69b80ab5010f3a5b9e189ee8d5b9
weshy007 <josephwaweru96@gmail.com>
2021-02-15 13:11:48 +0300

    the decryption works

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,42 @@
+import java.io.Console;
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What plain statement do you want to have encrypted...");
+        String plain = scan.nextLine();
+        System.out.println(" Specify your the value of change you'd ike on your text... ");
+        int change = scan.nextInt();
+        String encrypted = "";
+        char alphabet;
+        for(int i=0; i < plain.length();i++)
+        {
+            alphabet = plain.charAt(i);
+
+            if (alphabet>= 'a' && alphabet<= 'z')
+            {
+                alphabet = (char) (alphabet + change);
+
+                if(alphabet> 'z') {
+                    alphabet = (char) (alphabet+'a'-'z'-1);
+                }
+                encrypted = encrypted + alphabet;
+            }
+
+            else if (alphabet>= 'A' && alphabet<= 'Z')
+            {
+                alphabet = (char) (alphabet + change);
+
+                if(alphabet> 'Z') {
+                    alphabet = (char) (alphabet+'A'-'Z'-1);
+                }
+                encrypted = encrypted + alphabet;
+            }
+            else  {
+                encrypted = encrypted + alphabet;
+            }
+        }
+        System.out.println("This is your encrypted text: " + encrypted);
+    }
 }
